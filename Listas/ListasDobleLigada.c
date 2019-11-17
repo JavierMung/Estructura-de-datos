44921967fda10f86c0db9c2f89f5d7506ad9745c
Javier <javiermunguia35@gmail.com>
2019-11-16 18:38:42 -0700

    Listas modificadas, todas indentadas

--- Listas/ListasDobleLigada.c
+++ Listas/ListasDobleLigada.c
@@ -2,184 +2,251 @@
 #include <stdlib.h>
 
 
-struct Listadoble{
-	int dato;
-	struct Listadoble *siguiente;
-	struct Listadoble *anterior;
-	
+struct Listadoble
+{
+  int dato;
+  struct Listadoble *siguiente;
+  struct Listadoble *anterior;
+
 };
 
 
 
-int comprobar(struct Listadoble *cabeza, int dato){
-	
-	struct Listadoble *aux;
-	aux= NULL;
-	aux= cabeza;
-	
-	if (cabeza==NULL){
-		return 0;
-	}
-	else{
-		
-		while(aux->siguiente!=NULL){
-			if(aux->dato==dato){
-				return 1;
-			}
-			aux=aux->siguiente;
-			
-		}
-		if(aux->dato==dato){
-			return 1;
-		}
-	}
-	
-	return 0;
-	
-	
-	
-	
-	
-}
-void eliminar(struct Listadoble** cabeza, int dato){
-	struct Listadoble *aux, *cabesa;
+int
+comprobar (struct Listadoble *cabeza, int dato)
+{
 
-	cabesa=NULL;
-	aux=NULL;
-	cabesa= *cabeza;
-	if((*cabeza)->siguiente==NULL){
-	if ((*cabeza)->dato==dato){
-		aux= *cabeza;
-		*cabeza= NULL;
-		free(aux);
-	}
-  }else{
-  	
-  	if((*cabeza)->dato==dato){
-  	    aux= *cabeza;
-		(*cabeza)->siguiente->anterior=*cabeza;
-		*cabeza= (*cabeza)->siguiente;
-		free(aux);
-	  }
-	  else{
-	  	while(cabesa->dato!=dato){
-	  		cabesa=cabesa->siguiente;
-		  }
-		  if(cabesa->siguiente==NULL){
-		  	aux=cabesa;
-		  	cabesa->anterior->siguiente=NULL;
-		  	free(aux);
-		  }
-		  else{
-		  
-		  aux=cabesa;
-		  cabesa->anterior->siguiente=cabesa->siguiente;
-		  cabesa->siguiente->anterior=cabesa->anterior;
-		  free(aux);
-   	}
-   } 
-  }
-	
+  struct Listadoble *aux;
+  aux = NULL;
+  aux = cabeza;
 
-	
-}
-void ingresar_final(struct Listadoble **cabeza, int dato){
-	struct Listadoble* aux, *cabesa;
-	aux=NULL;
-	cabesa= NULL;
-	aux= (struct Listadoble*)malloc(sizeof(struct Listadoble));
-	cabesa=*cabeza;
-	
-	while(cabesa->siguiente!=NULL){
-		cabesa= cabesa->siguiente;
+  if (cabeza == NULL)
+    {
+      return 0;
+    }
+  else
+    {
+
+      while (aux->siguiente != NULL)
+	{
+	  if (aux->dato == dato)
+	    {
+	      return 1;
+	    }
+	  aux = aux->siguiente;
+
 	}
-	
-	aux->dato= dato;
-	aux->siguiente= NULL;
-	aux->anterior= cabesa;
-	cabesa->siguiente=aux;
-	
-	
-	
-}
-void mostrar(struct Listadoble *cabeza){
-	while(cabeza!=NULL){
-		printf("%d ->", cabeza->dato);
-		cabeza= cabeza->siguiente;
+      if (aux->dato == dato)
+	{
+	  return 1;
 	}
-	printf(" \n");
+    }
+
+  return 0;
+
+
+
+
 
 }
-void mostrar_atras(struct Listadoble *cabeza){
-	
-	struct Listadoble *atras;
-	atras = NULL;
-	atras = cabeza;
-	while(atras->siguiente!=NULL){
-		
-		atras= atras->siguiente;
-	}
-	
-	while(atras!= NULL){
-		printf("%d <- ", atras->dato);
-		atras= atras->anterior;
-	}
-	printf("\n");
-}
-void ingresar (struct Listadoble **cabeza, int dato){
-	struct Listadoble *nuevo;
-	nuevo=NULL;
-	nuevo = (struct Listadoble* )malloc(sizeof(struct Listadoble));
 
-	if(nuevo==NULL){
-		return ;
+void
+eliminar (struct Listadoble **cabeza, int dato)
+{
+  struct Listadoble *aux, *cabesa;
+
+  cabesa = NULL;
+  aux = NULL;
+  cabesa = *cabeza;
+  if ((*cabeza)->siguiente == NULL)
+    {
+      if ((*cabeza)->dato == dato)
+	{
+	  aux = *cabeza;
+	  *cabeza = NULL;
+	  free (aux);
 	}
-	else{
-		if (*cabeza==NULL){
-			nuevo->dato= dato;
-			nuevo->siguiente=NULL;
-			nuevo->anterior= NULL;;
-			*cabeza= nuevo;
-		}
-		else{
-			
-			nuevo->dato= dato;
-			nuevo->siguiente=*cabeza;
-			nuevo->anterior= NULL;
-			(*cabeza)->anterior= nuevo;
-			*cabeza =nuevo;
-		}
-		
+    }
+  else
+    {
+
+      if ((*cabeza)->dato == dato)
+	{
+	  aux = *cabeza;
+	  (*cabeza)->siguiente->anterior = *cabeza;
+	  *cabeza = (*cabeza)->siguiente;
+	  free (aux);
 	}
+      else
+	{
+	  while (cabesa->dato != dato)
+	    {
+	      cabesa = cabesa->siguiente;
+	    }
+	  if (cabesa->siguiente == NULL)
+	    {
+	      aux = cabesa;
+	      cabesa->anterior->siguiente = NULL;
+	      free (aux);
+	    }
+	  else
+	    {
+
+	      aux = cabesa;
+	      cabesa->anterior->siguiente = cabesa->siguiente;
+	      cabesa->siguiente->anterior = cabesa->anterior;
+	      free (aux);
+	    }
+	}
+    }
+
+
+
 }
 
-int main(void){
-	int dato=0, op= 0;
-	struct Listadoble *cabeza;
-	cabeza= NULL;
-	
-	
-	printf(" 1. Ingresar dato al principio. \n 2. Ingresar dato al final \n 3. Motrar desde el frente \n 4. Mostrar desde el final \n 5. Eliminar dato. \n 6. Salir del programa. \n\n ");
-	while(op!=6){
-			printf("\t\t\t\t***********ELIJE LA OPCION****************: ");
-			scanf("%d", &op);
-		switch(op){
-			
-			case 1: printf("Dame el dato : "); scanf("%d", &dato); ingresar(&cabeza, dato); break;
-			case 2:	printf("Dame el dato : "); scanf("%d", &dato); ingresar_final(&cabeza, dato); break;
-			case 3: mostrar(cabeza); break;
-			case 4: mostrar_atras(cabeza); break;
-			case 5: printf("Dame el dato que deseas eliminar: "); scanf("%d", &dato); comprobar(cabeza, dato); if(comprobar(cabeza, dato) == 1){
-				 eliminar(&cabeza, dato); break;
-			}
-			else {
-				printf("**********ERROR : NO EXISTE EL DATO A ELIMINAR*********\n");
-				break;
-			}
-			case 6: printf("\n/////////////FIN DEL PROGRAMA///////////////"); exit(-1);
-			
-		}
+void
+ingresar_final (struct Listadoble **cabeza, int dato)
+{
+  struct Listadoble *aux, *cabesa;
+  aux = NULL;
+  cabesa = NULL;
+  aux = (struct Listadoble *) malloc (sizeof (struct Listadoble));
+  cabesa = *cabeza;
+
+  while (cabesa->siguiente != NULL)
+    {
+      cabesa = cabesa->siguiente;
+    }
+
+  aux->dato = dato;
+  aux->siguiente = NULL;
+  aux->anterior = cabesa;
+  cabesa->siguiente = aux;
+
+
+
+}
+
+void
+mostrar (struct Listadoble *cabeza)
+{
+  while (cabeza != NULL)
+    {
+      printf ("%d ->", cabeza->dato);
+      cabeza = cabeza->siguiente;
+    }
+  printf (" \n");
+
+}
+
+void
+mostrar_atras (struct Listadoble *cabeza)
+{
+
+  struct Listadoble *atras;
+  atras = NULL;
+  atras = cabeza;
+  while (atras->siguiente != NULL)
+    {
+
+      atras = atras->siguiente;
+    }
+
+  while (atras != NULL)
+    {
+      printf ("%d <- ", atras->dato);
+      atras = atras->anterior;
+    }
+  printf ("\n");
+}
+
+void
+ingresar (struct Listadoble **cabeza, int dato)
+{
+  struct Listadoble *nuevo;
+  nuevo = NULL;
+  nuevo = (struct Listadoble *) malloc (sizeof (struct Listadoble));
+
+  if (nuevo == NULL)
+    {
+      return;
+    }
+  else
+    {
+      if (*cabeza == NULL)
+	{
+	  nuevo->dato = dato;
+	  nuevo->siguiente = NULL;
+	  nuevo->anterior = NULL;;
+	  *cabeza = nuevo;
 	}
-	return 0;
-	
+      else
+	{
+
+	  nuevo->dato = dato;
+	  nuevo->siguiente = *cabeza;
+	  nuevo->anterior = NULL;
+	  (*cabeza)->anterior = nuevo;
+	  *cabeza = nuevo;
+	}
+
+    }
+}
+
+int
+main (void)
+{
+  int dato = 0, op = 0;
+  struct Listadoble *cabeza;
+  cabeza = NULL;
+
+
+  printf
+    (" 1. Ingresar dato al principio. \n 2. Ingresar dato al final \n 3. Motrar desde el frente \n 4. Mostrar desde el final \n 5. Eliminar dato. \n 6. Salir del programa. \n\n ");
+  while (op != 6)
+    {
+      printf ("\t\t\t\t***********ELIJE LA OPCION****************: ");
+      scanf ("%d", &op);
+      switch (op)
+	{
+
+	case 1:
+	  printf ("Dame el dato : ");
+	  scanf ("%d", &dato);
+	  ingresar (&cabeza, dato);
+	  break;
+	case 2:
+	  printf ("Dame el dato : ");
+	  scanf ("%d", &dato);
+	  ingresar_final (&cabeza, dato);
+	  break;
+	case 3:
+	  mostrar (cabeza);
+	  break;
+	case 4:
+	  mostrar_atras (cabeza);
+	  break;
+	case 5:
+	  printf ("Dame el dato que deseas eliminar: ");
+	  scanf ("%d", &dato);
+	  comprobar (cabeza, dato);
+	  if (comprobar (cabeza, dato) == 1)
+	    {
+	      eliminar (&cabeza, dato);
+	      break;
+	    }
+	  else
+	    {
+	      printf
+		("**********ERROR : NO EXISTE EL DATO A ELIMINAR*********\n");
+	      break;
+	    }
+	case 6:
+	  printf ("\n/////////////FIN DEL PROGRAMA///////////////");
+	  exit (-1);
+
+	}
+    }
+  return 0;
+
 }
--- Listas/contigua.c
+++ Listas/contigua.c
@@ -1,143 +1,146 @@
-#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-
-struct Lista{
-	
-	int *dato;
-	int actual;
-	int cuantos;
-	
-	
+struct Lista
+{
+  int *dato;
+  int actual;
+  int cuantos;
 };
-
-struct Lista* crearnodo(int);
-void insertar(struct Lista** , int);
-void eliminar(struct Lista**, int);
-void mostrar (struct Lista*);
-
-int main(void) {
-	int dato;
-	struct Lista* lista;
-	lista=NULL;
-
-	insertar(&lista, 3);
-	insertar(&lista, 25);
-		insertar(&lista, 35);
-			insertar(&lista, 85);
-
-
-
-
-	eliminar(&lista, 35);
-	mostrar(lista);
-	
-	return 0;
+struct Lista *crearnodo (int);
+void insertar (struct Lista **, int);
+void eliminar (struct Lista **, int);
+void mostrar (struct Lista *);
+int
+main (void)
+{
+  int dato;
+  struct Lista *lista;
+  lista = NULL;
+  insertar (&lista, 3);
+  insertar (&lista, 25);
+  insertar (&lista, 35);
+  insertar (&lista, 85);
+  eliminar (&lista, 35);
+  mostrar (lista);
+  return 0;
 }
 
-struct Lista* crearnodo(int dato){
-	struct Lista* nuevo;
-	nuevo=NULL;
-	nuevo=(struct Lista*)malloc(sizeof(struct Lista));
-		if (nuevo==NULL){
-		return NULL;
-	}
-	nuevo->dato=(int *)malloc(8*(sizeof(int)));
-		if (nuevo->dato==NULL){
-		return NULL;
-	}
-	nuevo->actual=0;
-	nuevo->cuantos=8;
-	*(nuevo-> dato) = dato;
-	return nuevo;
-}
-void insertar(struct Lista** lista, int dato){
-	int i;
-	int *aux;
-	i=0;
-	aux=NULL;
-	
-	if (*lista==NULL){
-		*lista = crearnodo(dato);
-		if((*lista)==NULL){
-			return;
-		}
-		return;
-	}
-	
-	if((*lista)->cuantos==(*lista)->actual+1){
-		aux = (int *) malloc (sizeof(int) * ((*lista)->cuantos * 2) );
-		
-		if(aux==NULL){
-			return;
-		}
-		
-		for(i=0; i<(*lista)->cuantos; i++){
-			
-			*(aux +i)=*((*lista)->dato+i);			
-			
-		}	
-		
-		free((*lista)->dato);
-		(*lista)->dato=aux;
-		(*lista)->cuantos=((*lista)->cuantos)*2;
-		
-	}
-	
-	
-	
-	(*lista)->actual ++ ; //=(*lista)->actual+1;
-	
-	*((*lista)->dato + (*lista)->actual)=dato;
- 
+struct Lista *
+crearnodo (int dato)
+{
+  struct Lista *nuevo;
+  nuevo = NULL;
+  nuevo = (struct Lista *) malloc (sizeof (struct Lista));
+  if (nuevo == NULL)
+    {
+      return NULL;
+    }
+  nuevo->dato = (int *) malloc (8 * (sizeof (int)));
+  if (nuevo->dato == NULL)
+    {
+      return NULL;
+    }
+  nuevo->actual = 0;
+  nuevo->cuantos = 8;
+  *(nuevo->dato) = dato;
+  return nuevo;
 }
 
-void mostrar(struct Lista* lista){
-	int i;
-	if (lista==NULL){
-		printf("No hay datos. ");
-		return;
+void
+insertar (struct Lista **lista, int dato)
+{
+  int i;
+  int *aux;
+  i = 0;
+  aux = NULL;
+  if (*lista == NULL)
+    {
+      *lista = crearnodo (dato);
+      if ((*lista) == NULL)
+	{
+	  return;
 	}
-	else{
-		for(i=0; i<=lista->actual; i++){
-			printf(" %d ->", *(lista->dato+i));
-		}
+      return;
+    }
+  if ((*lista)->cuantos == (*lista)->actual + 1)
+    {
+      aux = (int *) malloc (sizeof (int) * ((*lista)->cuantos * 2));
+      if (aux == NULL)
+	{
+	  return;
 	}
+      for (i = 0; i < (*lista)->cuantos; i++)
+	{
+	  *(aux + i) = *((*lista)->dato + i);
+	}
+      free ((*lista)->dato);
+      (*lista)->dato = aux;
+      (*lista)->cuantos = ((*lista)->cuantos) * 2;
+    }
+  (*lista)->actual++;		//=(*lista)->actual+1;
+  *((*lista)->dato + (*lista)->actual) = dato;
 }
 
-void eliminar(struct Lista** lista, int dato){
-	int i=0;
-	int *aux=NULL;
-	
-	if(*lista==NULL){
-		return;
-	} 
-	if((*lista)->dato==NULL){
-		return;
-	}
-	
-	for (i=0; i<(*lista)->actual; i++){
-		if (*((*lista)->dato+i)==dato){
-			(*lista)->actual--;
-			break;
-		}
-	}
-	for(i; i<= (*lista)->actual; i++){
-		*((*lista)-> dato+i) = *((*lista)->dato+(i+1));
-	}
-	if((*lista)->actual<(*lista)->cuantos/2){
-		aux=(int*) malloc (sizeof (int) * ( (*lista)->cuantos/2) );
-		if(aux==NULL){
-			return;
-		}else{
-			for (i=0; i<=(*lista)->actual;i++){
-				*(aux+i)=*((*lista)->dato+i);
-			}
-			free((*lista)->dato);
-			(*lista)->dato=aux;
-			(*lista)->cuantos=(*lista)->cuantos/2;
-		}
-	}
+void
+mostrar (struct Lista *lista)
+{
+  int i;
+  if (lista == NULL)
+    {
+      printf ("No hay datos. ");
+      return;
+    }
 
-	
+  else
+    {
+      for (i = 0; i <= lista->actual; i++)
+	{
+	  printf (" %d ->", *(lista->dato + i));
+	}
+    }
+}
+
+void
+eliminar (struct Lista **lista, int dato)
+{
+  int i = 0;
+  int *aux = NULL;
+  if (*lista == NULL)
+    {
+      return;
+    }
+  if ((*lista)->dato == NULL)
+    {
+      return;
+    }
+  for (i = 0; i < (*lista)->actual; i++)
+    {
+      if (*((*lista)->dato + i) == dato)
+	{
+	  (*lista)->actual--;
+	  break;
+	}
+    }
+  for (i; i <= (*lista)->actual; i++)
+    {
+      *((*lista)->dato + i) = *((*lista)->dato + (i + 1));
+    }
+  if ((*lista)->actual < (*lista)->cuantos / 2)
+    {
+      aux = (int *) malloc (sizeof (int) * ((*lista)->cuantos / 2));
+      if (aux == NULL)
+	{
+	  return;
+	}
+      else
+	{
+	  for (i = 0; i <= (*lista)->actual; i++)
+	    {
+	      *(aux + i) = *((*lista)->dato + i);
+	    }
+	  free ((*lista)->dato);
+	  (*lista)->dato = aux;
+	  (*lista)->cuantos = (*lista)->cuantos / 2;
+	}
+    }
 }
